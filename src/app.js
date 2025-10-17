e83c6b9802ef9ea08e71853dce8a227fd018c1df
Gulshan-00 <rr3870044@gmail.com>
2025-10-18 03:41:46 +0530

    express server is created

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express= require("express");
+
+const app=express();
+
+app.use("/signup",(req,res)=>{
+   res.send("Hello, I am Signup Page")
+})
+
+app.use("/login",(req,res)=>{
+   res.send("Hello, I am Login Page")
+})
+
+app.use("/",(req,res)=>{
+   res.send("Hello, I am Home Page")
+})
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server is successful running")
+});
